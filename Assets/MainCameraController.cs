a606da2690e9fe23a2d46472cee009a3483cc0b0
MapleDrizzle <siromero@udel.edu>
2025-02-07 09:51:20 -0500

    Commit MainCameraController.cs and its .meta

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("What kind of computer is the best singer? A Dell");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
